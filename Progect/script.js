6baf73b9fbe74c2db4c3a70692b441be7060eb50
Artem69AV <Artemburin69@gmail.com>
2019-12-08 16:13:46 +0300

    Это первый комит

---
+++ Progect/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let namber = 5;
+let string = 'text';
+let sym = Symbol();
+let boolean = true;
+null;
+undefined;
+let odj = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+let samesing;
+console.log(samesing);
+
+let person = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(person['name']);
+
+let arr = ['plam.png', 'orang.jpd', 'apple.bpm'];
+
+console.log(arr[0]);
+
+// alert('hellow world');
+
+// let question = +prompt('есть ли вам 18?', 'да');
+// console.log(typeof(question));
+
+// console.log('arr' + '- object');
+// console.log(4 + +' - object');
+
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log('2'===2);
+
+let inChecked = true,
+inClose = false;
+
+console.log(inChecked && inClose);
